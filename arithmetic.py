e2901e37087083f940bba34a51a6244ed034ba48
Rose Ganache <mrg2172@columbia.edu>
2013-06-18 16:07:07 -0700

    Arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,28 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return float(num1) / num2
+
+def square(num1):
+    return num1 * num1
+
+def cube(num1):
+    return num1 * num1 * num1
+
+def power(num1, num2):
+   return num1**num2
+
+   # i = 0
+    #for (i < num2):
+    #	num1 *= num1
+    #	i+=1
+
+def mod(num1, num2):
+    return num1 % num2
